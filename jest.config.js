476bf4fd15208120ffebabfbec5677c5f52d954f
zakawaki <encarnacionjose900@gmail.com>
2025-04-20 11:51:01 -0400

    Add GitHub Actions workflow

--- jest.config.js
+++ jest.config.js
@@ -10,3 +10,19 @@
       '/src/' 
     ]
   };
+  module.exports = {
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src', '<rootDir>/__tests__'],
+    modulePaths: ['<rootDir>'],
+    moduleDirectories: ['node_modules', 'src'],
+    collectCoverageFrom: ['src/**/*.js']
+  };
+  module.exports = {
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.test.js'],
+    verbose: true
+  };
+  module.exports = {
+    modulePaths: ['<rootDir>'],
+    moduleDirectories: ['node_modules', 'src'],
+  };
